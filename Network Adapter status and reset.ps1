7c2d9389eee624f0f8df75dd28ad3512655eab87
LucWizardinmyhead <lucasjon817@gmail.com>
2024-09-30 22:11:00 -0700

    Add files via upload

---
+++ Network Adapter status and reset.ps1
@@ -0,0 +1,20 @@
+﻿#Grab all network Adapters 
+
+$networkAdapters = Get-NetAdapter
+
+#Display Adapter status
+
+$networkAdapters |Format-Table Name, Status, MacAddress, LinkSpeed
+
+#Reset Network Adapter if its down/disconnected
+
+$networkAdapters | Where-Object { $_.Status -ne "Up"} | ForEach-Object {
+    Write-Host "Resetting adapter: $($_.Name)"
+    Disable-NetAdapter -Name $_.Name -Confirm:$false
+    Start-Sleep -Seconds 5
+    Enable-NetAdapter -Name $_.Name -Confirm:$false
+
+}
+
+
+
